1c1a9d94adff2d2864a228c586945cb641250f5e
bielsilva77 <gabrielpinheiro2905@gmail.com>
2025-03-19 19:28:31 -0300

    Add files via upload

---
+++ FUNÇÃO_FOR-GABRIEL.py
@@ -0,0 +1,12 @@
+#1
+for i in range(1, 51, 2):  # Começa de 1, vai até 50 e pula de 2 em 2
+    print(i)
+
+#2
+for i in range(1, 41, 3):  # Começa de 1, vai até 40 e pula de 3 em 3
+    print(i)
+
+#3
+    for i in range(0, 31, 2):  # Começa de 0, vai até 30 e pula de 2 em 2
+    if i not in [10, 20, 30]:  # Exclui os números 10, 20 e 30
+        print(i)
